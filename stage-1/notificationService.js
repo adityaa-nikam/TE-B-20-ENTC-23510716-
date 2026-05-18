c3a36373cd5cad998e2e59b190bec0075cbab013
adityaa-nikam <adiityaa.nikam@gmail.com>
2026-05-18 16:39:22 +0530

    Completed Stage 1 notification priority system

--- stage-1/notificationService.js
+++ stage-1/notificationService.js
@@ -8,15 +8,16 @@ async function fetchNotifications() {
         log("Fetching notifications from API");
 
         const response = await axios.get(
-            "http://4.224.186.213/evaluation-service/notifications"
+            "http://4.224.186.213/evaluation-service/notifications",
+            {
+                headers: {
+                    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyMzUxMDcxNi5keXBpdEBkeXB2cC5lZHUuaW4iLCJleHAiOjE3NzkxMDA0NjQsImlhdCI6MTc3OTA5OTU2NCwiaXNzIjoiQWZmb3JkIE1lZGljYWwgVGVjaG5vbG9naWVzIFByaXZhdGUgTGltaXRlZCIsImp0aSI6IjgzNzE1YWFmLTNjNDgtNDVmNi05NzBhLTAxYzJiNTE4ZDBhZCIsImxvY2FsZSI6ImVuLUlOIiwibmFtZSI6ImFkaXR5YSBuaWthbSIsInN1YiI6IjFiZDQ4YmQwLWI2M2EtNDA0Mi04NjVhLTVjYzM5ODFiMDZkNyJ9LCJlbWFpbCI6IjIzNTEwNzE2LmR5cGl0QGR5cHZwLmVkdS5pbiIsIm5hbWUiOiJhZGl0eWEgbmlrYW0iLCJyb2xsTm8iOiIyMzUxMDcxNiIsImFjY2Vzc0NvZGUiOiJmekVRU1EiLCJjbGllbnRJRCI6IjFiZDQ4YmQwLWI2M2EtNDA0Mi04NjVhLTVjYzM5ODFiMDZkNyIsImNsaWVudFNlY3JldCI6IlBUWFZNRmZ2SHh6WXpUaHgifQ.Ttgf8tRClR3zLufFoX88LC6-iIjCxdoeAvJ_SZqnz5I"
+                }
+            }
         );
 
         log("Notifications fetched successfully");
 
-        // TEMP DEBUG
-        console.log(response.data);
-
-        // Handle multiple possible response structures
         if (response.data.notifications) {
             return response.data.notifications;
         }
@@ -30,8 +31,6 @@ async function fetchNotifications() {
     } catch (error) {
 
         log(`API Error: ${error.message}`);
-
-        console.log(error.message);
 
         return [];
     }
